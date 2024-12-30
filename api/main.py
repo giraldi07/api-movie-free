9154f3b072bf739e1e073c9270d2f6c250a3fd13
giraldi07 <giraldipramay@gmail.com>
2024-12-30 16:34:36 +0700

    update CORS

--- api/main.py
+++ api/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import movies, tvseries, genres, releases
 
 app = FastAPI()
 
-# Daftarkan rute
+# Define the allowed origins (You can specify your front-end domains or use '*' for all origins)
+origins = [
+    "http://localhost",  # For local development
+    "http://localhost:3000",  # If your front-end is on port 3000 (e.g., React app)
+    "https://api-movie-peach.vercel.app/",  # Replace with your actual production domain
+]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows only the listed domains (or '*' for all)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+# Register routes
 app.include_router(movies.router, prefix="/movies", tags=["Movies"])
 app.include_router(tvseries.router, prefix="/tvseries", tags=["TV Series"])
 app.include_router(genres.router, prefix="/genres", tags=["Genres"])
@@ -13,4 +30,3 @@ app.include_router(releases.router, prefix="/releases", tags=["Releases"])
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Movie API"}
-
