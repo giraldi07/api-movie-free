c6b8cfedf878eaae149747b7206c20fde3ccc19d
giraldi07 <giraldipramay@gmail.com>
2024-12-30 16:46:48 +0700

    update CORSs

--- api/main.py
+++ api/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 # Define the allowed origins (You can specify your front-end domains or use '*' for all origins)
 origins = [
-    "http://localhost",  # For local development
+    "http://localhost:5173",  # For local development
     "http://localhost:3000",  # If your front-end is on port 3000 (e.g., React app)
     "https://api-movie-peach.vercel.app/",  # Replace with your actual production domain
 ]
